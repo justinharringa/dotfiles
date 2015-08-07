57048b5820ffb6b0609672019e36d8d2132ad443
Justin Harringa <justin@harringa.com>
2015-08-07 16:59:58 -0300

    Add time to prompt... still contemplating oh-my-zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -71,7 +71,11 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+time_info() {
+  echo "%{$fg_bold[green]%}[%*]%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(time_info) $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
