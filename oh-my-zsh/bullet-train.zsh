bb1e3b6ea2c9eeeb2ee0ae7f8c3a5a6539ee99bd
Justin Harringa <justin@harringa.com>
2021-02-21 11:33:29 -0800

    Update Brewfile and oh-my-zsh/bullet-train.zsh
    Brewfile
    * remove unrar (doesn't work)
    * remove atom and virtualbox (don't really use)
    
    Take ruby/perl out of bullet-train and add kctx

--- oh-my-zsh/bullet-train.zsh
+++ oh-my-zsh/bullet-train.zsh
@@ -1,1 +1,17 @@
-export BULLETTRAIN_RUBY_SHOW=false
+export BULLETTRAIN_PROMPT_ORDER=(
+  time
+  status
+  custom
+  context
+  dir
+  screen
+  virtualenv
+  aws
+  go
+  rust
+  elixir
+  git
+  kctx
+  cmd_exec_time
+)
+
