c2ff711ce766ac1d9e7ceea93529cfd0ddb3e2fe
Justin Harringa <justin@harringa.com>
2022-06-14 14:48:06 -0700

    Only run gcloud stuff if SDK installed

--- gcloud/source.zsh
+++ gcloud/source.zsh
@@ -1,2 +1,5 @@
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+if [[ -d "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/" ]]
+then
+  source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
