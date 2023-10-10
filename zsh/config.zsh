15a30293c9c5ce3f1816dbd2b185d746d21f7fc5
Justin Harringa <justin@harringa.com>
2023-10-10 11:59:18 -0700

    Remove LOCAL_OPTIONS/LOCAL_TRAPS opts from config
    https://github.com/ohmyzsh/ohmyzsh/issues/11767

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -12,8 +12,6 @@ SAVEHIST=10000
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
